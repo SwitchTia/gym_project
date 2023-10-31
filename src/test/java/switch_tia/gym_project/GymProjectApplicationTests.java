6e8634f59da74a5a43172767ee01d44dfe202c0c
SwitchTia <oblasser.tatiana@gmail.com>
2023-10-31 17:34:12 +0100

    31oct beginning Project

---
+++ src/test/java/switch_tia/gym_project/GymProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package switch_tia.gym_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
