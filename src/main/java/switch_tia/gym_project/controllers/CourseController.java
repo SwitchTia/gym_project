7d56c56c87c59a69975940cb5bf2c7b468303136
SwitchTia <oblasser.tatiana@gmail.com>
2023-11-03 15:48:14 +0100

    course methods

--- src/main/java/switch_tia/gym_project/controllers/CourseController.java
+++ src/main/java/switch_tia/gym_project/controllers/CourseController.java
@@ -1,10 +1,51 @@
 package switch_tia.gym_project.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.config.Task;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import switch_tia.gym_project.entities.Course;
+import switch_tia.gym_project.repositories.CourseRepository;
+import switch_tia.gym_project.services.CourseService;
 
 @RestController
 @RequestMapping ("/course")
 public class CourseController {
+    
+    @Autowired
+    CourseRepository cr;
+
+    @Autowired
+    CourseService cs;
+
+    @PostMapping ("/saveCourse")
+    @PreAuthorize ("hasAnyAuthority ('ADMIN','INSTRUCTOR')")
+    public ResponseEntity saveCourse (@RequestBody Course c){
+        try {
+            return new ResponseEntity (cs.saveCourse (c), HttpStatus.OK);
+        } catch (RuntimeException e) {
+            String ex = e.getClass().getSimpleName();
+            return new ResponseEntity (ex, HttpStatus.BAD_REQUEST);
+        }
+    }
+    
+    @DeleteMapping ("/deleteCourse")
+    @PreAuthorize ("hasAnyAuthority ('ADMIN','INSTRUCTOR')")
+    public ResponseEntity deleteCourse (@RequestParam ("courseCode") int courseCode){
+        try {
+            return new ResponseEntity (cs.deleteCourse (courseCode), HttpStatus.OK);
+        } catch (RuntimeException e) {
+            String ex = e.getClass().getSimpleName();
+            return new ResponseEntity (ex, HttpStatus.BAD_REQUEST);
+        }
+    }
     
 }
--- src/main/java/switch_tia/gym_project/services/CourseService.java
+++ src/main/java/switch_tia/gym_project/services/CourseService.java
@@ -1,5 +1,34 @@
 package switch_tia.gym_project.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import switch_tia.gym_project.UTILITIES.exceptions.CourseAlreadyExistsException;
+import switch_tia.gym_project.UTILITIES.exceptions.IncorrectDataException;
+import switch_tia.gym_project.entities.Course;
+import switch_tia.gym_project.repositories.CourseRepository;
+
+@Service
 public class CourseService {
+
+    @Autowired
+    CourseRepository cr;
+
+    public Course saveCourse (Course c) throws RuntimeException{
+        if (cr.existsByCourseCode(c.getCourseCode())){
+            throw new CourseAlreadyExistsException ();
+        }
+        return cr.save(c);
+    }
+
+    public String deleteCourse (int courseCode) throws RuntimeException {
+        Course c = cr.findByCourseCode(courseCode);
+        if (c == null) {
+            throw new IncorrectDataException();
+        }
+        cr.delete(c);
+        String x = "The course has been deleted";
+        return x;
+    }
     
 }
